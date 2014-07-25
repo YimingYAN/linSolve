337fa1de9badf13ba0c8e5d62a20c50ce37f78d7
Yiming Yan <yanyiming1986@gmail.com>
2014-07-25 12:27:46 +0100

    minor update: added #ifdef

--- cma27.h
+++ cma27.h
@@ -3,9 +3,11 @@
 * Created by Yiming Yan 
 * 25 July 2014
 */
+#ifdef __cplusplus
 extern "C" {
+#endif
     extern void ma27id_(int icntl[30], double cntl[5]);
-    /* MA27ID_ C++ interface for fortran routine MA27ID (Initialize)
+    /* MA27ID_ C interface for fortran routine MA27ID (Initialize)
      * icntl    -
      * cntl     -
      */
@@ -14,7 +16,7 @@ extern "C" {
                         int *liw, int *ikeep, int *iw1, int *nsteps,
                         int *iflag, int icntl[30], double cntl[5],
                         int info[20], double *ops );
-    /* MA27AD_ C++ interface for fortran routine MA27AD (Analyze)
+    /* MA27AD_ C interface for fortran routine MA27AD (Analyze)
      * n        - order of matrix, namely number of rows or cols
      * nz       - number of nonzeros entries
      * irn      - row index of input
@@ -35,7 +37,7 @@ extern "C" {
                    double *a, int *la, int iw[], int *liw,
                    int *ikeep, int *nsteps, int *maxfrt, int *iw1,
                    int icntl[30], double cntl[5], int info[20]);
-    /* MA27BD_ C++ interface for fortran routine MA27BD (Factorize)
+    /* MA27BD_ C interface for fortran routine MA27BD (Factorize)
      * a        - data array
      * la       - length of a
      * maxfrt   - integer, to be set by ma27
@@ -44,10 +46,12 @@ extern "C" {
     extern void ma27cd_(int *n, double *a, int *la, int iw[], int *liw,
                        double *w, int *maxfrt, double *rhs, int *iw1,
                        int *nsteps, int icntl[30], int info[20]);
-    /* MA27CD_ C++ interface for fortran routine MA27CD (Solve)
+    /* MA27CD_ C interface for fortran routine MA27CD (Solve)
      * w        - double workspace
      * rhs      - right hand side of the linear system (b in Ax=b);
      *            after the solving process, rhs contains the solution.
      */
+#ifdef __cplusplus
 }
+#endif
 
