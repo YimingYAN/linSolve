ba84a8c49b9fb79438f374b6ef98111411c07c64
Yiming Yan <yanyiming1986@gmail.com>
2014-07-30 00:04:35 +0100

    finished the main interface for fortran routines in ma57d.f

--- example.cpp
+++ example.cpp
@@ -93,11 +93,31 @@ int main (int argc, char *argv[])
     int icntl57[20];
     double cntl57[5];
     int lkeep57 = 100;
-    int keep57[lkeep];
+    int keep57[lkeep57];
     int iwork57[5*n];
     int info57[40];
     double rinfo57[20];
+    int lfact57 = 1000;
+    double fact57[lfact57];
+    int lifact57 = 500;
+    int ifact57[lifact57];
+    int nrhs57 = 1, lrhs57 = 5, lw57 = 40, job57 = 1;
+    double w57[lw57];
     
+    // reset a and rhs
+    counter = 0;
+    for (int i=0; i<nz; i++)
+    {
+        for (int j=i; j<n; j++)
+        {
+            if (fabs(A[i][j]) > 1e-32)
+            {
+                a[counter] = A[i][j];
+                counter++;
+            }
+        }
+    }
+    rhs[0] = 8; rhs[1] = 45.; rhs[2] = 31.; rhs[3] = 15.; rhs[4] = 17.;
     
     /* ====== End Vars for MA57 ====== */
     
@@ -111,12 +131,16 @@ int main (int argc, char *argv[])
     
     
     // analyze
-    ma57ad_(&n, &nz, irn, icn, &lkeep57, keep57, iwork57,
+    ma57ad_(&n, &nz, irn, icn, &lkeep57, keep57, iw,
             icntl57, info57, rinfo57);
     
     // factorize
+    ma57bd_(&n, &nz, a, fact57, &lfact57, ifact57, &lifact57, &lkeep57,
+            keep57, iw, icntl57, cntl57, info57, rinfo57);
     
     // solve
+    ma57cd_(&job57, &n, fact57, &lfact57, ifact57, &lifact57, &nrhs57, rhs,
+            &lrhs57, w57, &lw57, iw, icntl57, info57);
     
     /* ====== End MA57 ====== */
     
--- include/cma57.h
+++ include/cma57.h
@@ -7,8 +7,13 @@
 extern "C" {
 #endif
     extern void ma57id_(double cntl[5], int icntl[20]);
+    /*
+     * cntl     - array of double; contains double control values
+     * icntl    - array of  integer; contaitns integer control values
+     */
+    
     extern void ma57ad_(int *n, int *ne, int *irn, int *jcn,
-                        int *lkeep, int *keep, int *iwork,
+                        int *lkeep, int *keep, int *iw,
                         int icntl[20], int info[40], double rinfo[20]);
     /*
      * n        - dimension of matrix A
@@ -16,16 +21,43 @@ extern "C" {
      * irn      - row index of input
      * jcn      - col index of input
      * lkeep    - length of array keep; 
-     *            lkeep ≥ 5*N+NE+MAX(N,NE)+42
+     *            lkeep ≥ 5*n+ne+MAX(n,ne)+42
      * keep     - integer array; pivot sequence
-     * iwork    - integer array of length 5*N; workspace
+     * iw       - integer array of length 5*N; workspace
      * info     - integer array of length 40
      * rinfo    - double array of length 20
      */
-    //extern void ma57bd_();
-    //extern void ma57cd_();
-    //extern void ma57dd_();
-    //extern void ma57ed_();
+    
+    extern void ma57bd_(int *n, int *ne, double *A, double *fact,
+                        int *lfact, int *ifact, int *lifact, int *lkeep,
+                        int *keep, int *iw, int icntl[20], double cntl[5],
+                        int info[40], double rinfo[20]);
+    /*
+     * A        - double array of data
+     * fact     - double array of length lfact; 
+     *            holds the entries of the factors of the matrix A
+     * lfact    - length of fact
+     * ifact    - integer array;
+                  holds integer indexing information on the matrix factors
+     * lifact   - length of ifact
+     */
+    
+    extern void ma57cd_(int *job, int *n, double *fact, int *lfact,
+                        int *ifact, int *lifact, int *nrhs, double *rhs,
+                        int *lrhs, double *w, int *lw, int *iw,
+                        int icntl[20], int info[40]);
+    /*
+     * job      - integer; determines the system being solved 
+     *            job = 1 if solving system AX=B
+     * w        - double array of length; workspace
+     * lw       - length of w; lw>=n*nrhs
+     * nrhs     - number of right hand side being solved
+     * rhs      - array of double; right hand side of the system
+     * lrhs     - integer, lenght of rhs
+     * w        - double array of length lw; workspace
+     * lw       - integer, length of w
+     * iw       - integer array of length n; workspace
+     */
 #ifdef __cplusplus
 }
 #endif
